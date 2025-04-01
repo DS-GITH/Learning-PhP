a16ca881898abf7124dc2716b58034e6d51318a1
David Santos <david.tecds@gmail.com>
2025-03-31 23:03:36 -0300

    Gettype para capturar o tipo de uma variável

--- Area51/Readline.php
+++ Area51/Readline.php
@@ -1,14 +1,15 @@
 <?php
 
-echo "Escreva seu nome: ";
+echo "Escreva seu nome: \n";
 $nome = readline();
 
-echo "Informe sua profissão: ";
+echo "Informe sua profissão: \n";
 $prof = readline();
 
-echo "Informe sua idade: ";
-$idade = readline();
+echo "Informe sua idade: \n";
+$idade = intval(readline());
 
-echo "O $nome que tem $idade anos, que trabalha no setor de $prof, se cadastrou!"
+echo "O $nome que tem $idade anos, que trabalha no setor de $prof, se cadastrou! \n";
+echo gettype($idade);
 
 ?>
