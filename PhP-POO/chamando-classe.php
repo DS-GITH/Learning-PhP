cb2bbcac12788dd2a93aca42251c105f7d17dc20
David Santos <david.tecds@gmail.com>
2025-04-15 14:03:36 -0300

    Iniciando POO com criação de classe e chamando-a

---
+++ PhP-POO/chamando-classe.php
@@ -0,0 +1,3 @@
+require_once "classe.php";
+
+$c = new classe("André", "Fossa");
---
+++ PhP-POO/classe.php
@@ -0,0 +1,15 @@
+<? php
+
+
+class Pessoa
+{
+
+public string $nome;
+
+public string $lugar;
+
+public cagar(){
+    return "O {$this->nome} está querendo soltar o barroso na {$this->lugar}"
+}
+
+}
